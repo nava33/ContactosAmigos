77c0a5983dbe0da9d998d2820963fa02b4c1bcaa
FlutLab.io <>
2021-05-02 20:40:23 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+  
+import 'package:flutter/material.dart';
+import 'contact_view.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: ContactsPage()
+    )
+  );
+}
